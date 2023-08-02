c51d6d39ce9a22b5645c74b20e274a7d6a3c0291
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-08-02 22:43:51 +0100

    adding the button into con and appending con to html doc and adding content to button

--- Javascript/script.js
+++ Javascript/script.js
@@ -26,14 +26,61 @@ const minus = document.createElement("button")
 const multiply = document.createElement("button")
 const divide = document.createElement("button")
 const equal = document.createElement("button")
+const allClear = document.createElement("button")
+// creating input con
+const input_con = document.createElement("div")
+
+// Giving the button content
+zero.textContent = 0
+one.textContent = 1
+two.textContent = 2
+three.textContent = 3
+four.textContent = 4
+five.textContent = 5
+six.textContent = 6
+seven.textContent = 7
+eight.textContent = 8
+nine.textContent = 9
+multiply.textContent = "X"
+add.textContent = "+"
+divide.textContent = "/"
+minus.textContent = "-"
+allClear.textContent = "A/C"
+equal.textContent = "="
+
+
+// adding button to input-con
+input_con.appendChild(seven)
+input_con.appendChild(eight)
+input_con.appendChild(nine)
+input_con.appendChild(add)
+input_con.appendChild(four)
+input_con.appendChild(five)
+input_con.appendChild(six)
+input_con.appendChild(multiply)
+input_con.appendChild(one)
+input_con.appendChild(two)
+input_con.appendChild(three)
+input_con.appendChild(minus)
+input_con.appendChild(zero)
+input_con.appendChild(divide)
+input_con.appendChild(allClear)
+input_con.appendChild(equal)
+
+
+
 
 // Giving the element classes
 contanier.classList.add("cal-con")
+
 currentInput.classList.add("current-con")
 lastInput.classList.add("last-con")
 display.classList.add("dis-con")
 
+input_con.classList.add("input-con")
 
 // adding the to the html page
 document.body.appendChild(contanier)
 contanier.appendChild(display)
+contanier.appendChild(input_con)
+
